19fbaeaeca34dca230f8844ce0856caa94828fe4
Andreas Eriksson <andreas@codered.se>
2021-09-22 08:31:43 +0200

    Add package information

--- mix.exs
+++ mix.exs
@@ -7,6 +7,12 @@ defmodule Brixx.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
+      name: "Brixx",
+      description: "UI Components for Phoenix and LiveView",
+      source_url: "https://github.com/brixx-ui/brixx",
+      homepage_url: "https://github.com/brixx-ui/brixx",
+      package: package(),
+      docs: docs(),
       deps: deps()
     ]
   end
@@ -23,6 +29,25 @@ defmodule Brixx.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+
+  defp package do
+    [
+      maintainers: ["Andreas Eriksson"],
+      licenses: ["MIT"],
+      links: %{
+        "Github" => "https://github.com/brixx-ui/brixx"
+      },
+      files: ~w(lib priv mix.exs README.md)
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      extras: ["README.md"]
     ]
   end
 end
